1c83723a62edb809f3cce612ecaf9d732a91ffe4
Arthur Borges <arthurmine96@gmail.com>
2025-06-03 09:55:12 -0300

    estrutura basica para o ranking

---
+++ plataforma_jogos/ranking.py
@@ -0,0 +1,7 @@
+class Ranking:
+    def __init__(self):
+        self.jogadores = [] # lista de objetos Jogador
+        
+        
+    def adicionar_jogador(self, jogador):
+        self.jogadores.append(jogador)
