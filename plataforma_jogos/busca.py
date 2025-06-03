16f439f9c28f383283c55047a68d5ffaa31b3318
Arthur Borges <arthurmine96@gmail.com>
2025-06-03 11:03:24 -0300

    correção codigo de busca

--- plataforma_jogos/busca.py
+++ plataforma_jogos/busca.py
@@ -13,16 +13,16 @@ def buscar_jogadores(nome, lista_jogadores):
         print("❌ Nenhum jogador encontrado com esse nome.")
         
         
-    def buscar_partidas (modo, lista_partidas):
-        print(f"\n🔍 Buscando partidas no modo: '{modo}'")
-        filtradas = []
+def buscar_partidas (modo, lista_partidas):
+    print(f"\n🔍 Buscando partidas no modo: '{modo}'")
+    filtradas = []
         
-        for partida in lista_partidas:
-            if partida.modo.lower() == modo.lower():
-                filtradas.append(partida)
+    for partida in lista_partidas:
+        if partida.modo.lower() == modo.lower():
+            filtradas.append(partida)
 
         if filtradas:
             for p in filtradas:
                 print(f"✅ Partida encontrada: {p.id} (Modo: {p.modo}) (Jogadores: {[j.nome for j in p.jogadores]}) Mapa: {p.mapa} - Ping: {p.ping}ms")
         else:
-            print("❌ Nenhuma partida encontrada nesse modo.")
+                print("❌ Nenhuma partida encontrada nesse modo.")
