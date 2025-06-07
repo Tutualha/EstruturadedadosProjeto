1cdef97c8b2bbbc3f739a95405b753b96566a3b8
Samuel Azevedo <samuzvd@gmail.com>
2025-06-06 22:32:35 -0300

    Update ranking.py
    Comentando o código

--- plataforma_jogos/ranking.py
+++ plataforma_jogos/ranking.py
@@ -1,12 +1,15 @@
 from jogador import Jogador
 
+# Mostra o Ranking e a posição dos jogadores 
 class Ranking:
     def __init__(self):
         self.jogadores = []  # lista de objetos Jogador
 
+    # Adiciona um jogador no Ranking
     def adicionar_jogador(self, jogador):
         self.jogadores.append(jogador)
 
+    # Atualiza as pontuações do jogador
     def atualizar_pntuação(self, nome, nova_pontuaçao):
         for j in self.jogadores:
             if j.nome.lower() == nome.lower():
@@ -15,14 +18,16 @@ class Ranking:
                 return
         print("Jogador não encontrado no ranking.")
 
+    # Mostra o top N jogadores com as maiores pontuações 
     def mostrar_top(self, n=10):
         top = sorted(self.jogadores, key=lambda j: j.pontos, reverse=True)[:n]
         print(f"Top {n} jogadores:")
         for i, j in enumerate(top, start=1):
             print(f"{i}. {j.nome} - {j.pontos} pts - {j.vitorias} vitórias")
 
+    # Busca um jogador expecífico
     def buscar_jogador(self, nome):
         for jogador in self.jogadores:
             if nome.lower() == jogador.nome.lower():
                 return jogador
-        return None
+        return None # Se nenhum jogador for encontrado
